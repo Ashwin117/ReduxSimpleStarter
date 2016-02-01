fc0aeb27475a00179cd74cd1e8f160b6dd744e0b
Ashwin Sundar <assundar@paypal.com>
2016-01-31 21:25:14 -0800

    Book list implementing reducer and redix stage 1

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{title: 'Javascrit: The Good Parts'},
+		{title: 'Harry Potter'},
+		{title: 'Lord of the Rings'},
+		{title: 'The Dark Side'},
+		{title: 'Eloquent Ruby'}
+	]
+}
