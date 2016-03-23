beb898aa14c4a9d5414bf63b2d7c9f3299dcca07
Ashwin Sundar <assundar@paypal.com>
2016-03-23 02:33:32 -0700

    Blog Project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
+import PostsReducer from './reducer_posts';
+// get the reducer property and create a variable named formReducer
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+	posts: PostsReducer,
+	//MUST USE KEY 'form'
+	form: formReducer
 });
 
 export default rootReducer;
